5a481c37ddb57ae5160edb6581ce51c50e7f3e56
davidesner <david@keboola.com>
2019-01-11 12:45:09 -0800

    initial commit

---
+++ src/component.py
@@ -0,0 +1,240 @@
+'''
+Template Component main class.
+
+'''
+
+import logging
+import os
+from datetime import datetime
+
+import pandas as pd
+from kbc.env_handler import KBCEnvHandler
+
+from hubspot.client_service import HubspotClientService
+
+# primary keys
+PIPELINE_STAGE_PK = ['PIPELINE_ID', 'stageId']
+PIPELINE_PK = ['pipelineId']
+OWNER_PK = ['ownerId']
+LISTS_PK = ['listId']
+ACTIVITIES_PK = ['engagement_id ']
+EMAIL_EVENTS_PK = ['id', 'created']
+CAMPAIGNS_PK = ['id']
+DEAL_C_LIST_PK = ['Deal_ID', 'Contact_ID']
+DEAL_STAGE_HIST_PK = ['DEAL_ID', 'sourceVid', 'sourceId', 'timestamp']
+DEAL_PK = ['dealId ']
+CONTACT_LIST_PK = ['internal_list_id', 'static_list_id', 'CONTACT_ID']
+C_SUBMISSION_PK = ['form_id', 'CONTACT_ID', 'portal_id', 'conversion_id', 'page_id']
+CONTACT_PK = ['vid', 'portal_id']
+COMPANY_ID_COL = ['companyId']
+
+# config keys
+KEY_API_TOKEN = '#api_token'
+KEY_PERIOD_FROM = 'period_from'
+KEY_PERIOD_TO = 'period_to'
+
+MANDATORY_PARS = [KEY_API_TOKEN]
+MANDATORY_IMAGE_PARS = []
+
+APP_VERSION = '0.0.1'
+
+
+class Component(KBCEnvHandler):
+
+    def __init__(self, debug=False):
+        KBCEnvHandler.__init__(self, MANDATORY_PARS)
+        # override debug from config
+        if self.cfg_params.get('debug'):
+            debug = True
+
+        self.set_default_logger('DEBUG' if debug else 'INFO')
+        logging.info('Running version %s', APP_VERSION)
+        logging.info('Loading configuration...')
+
+        try:
+            self.validate_config(MANDATORY_PARS)
+            self.validate_image_parameters(MANDATORY_IMAGE_PARS)
+        except ValueError as e:
+            logging.error(e)
+            exit(1)
+
+    def run(self):
+        '''
+        Main execution code
+        '''
+        params = self.cfg_params  # noqa
+        token = params[KEY_API_TOKEN]
+        client_service = HubspotClientService(token)
+
+        if params.get(KEY_PERIOD_FROM):
+            start_date, end_date = self.get_date_period_converted(params.get(KEY_PERIOD_FROM),
+                                                                  datetime.utcnow().strftime('%Y-%m-%d'))
+            recent = True
+        else:
+            start_date, end_date = None
+            recent = False
+
+        logging.info('Extracting Companies')
+        res_file_path = os.path.join(self.tables_out_path, 'companies.csv')
+        self._get_simple_ds(res_file_path, COMPANY_ID_COL, client_service.get_companies, recent)
+
+        logging.info('Extracting Campaigns from HubSpot CRM')
+        res_file_path = os.path.join(self.tables_out_path, 'campaigns.csv')
+        self._get_simple_ds(res_file_path, CAMPAIGNS_PK, client_service.get_campaigns, recent)
+
+        logging.info('Extracting Email Events from HubSpot CRM')
+        res_file_path = os.path.join(self.tables_out_path, 'email_events.csv')
+        self._get_simple_ds(res_file_path, EMAIL_EVENTS_PK, client_service.get_email_events, start_date)
+
+        logging.info('Extracting Activities from HubSpot CRM')
+        res_file_path = os.path.join(self.tables_out_path, 'activities.csv')
+        self._get_simple_ds(res_file_path, ACTIVITIES_PK, client_service.get_activities, start_date)
+
+        logging.info('Extracting Lists from HubSpot CRM')
+        res_file_path = os.path.join(self.tables_out_path, 'lists.csv')
+        self._get_simple_ds(res_file_path, LISTS_PK, client_service.get_lists)
+
+        logging.info('Extracting Owners from HubSpot CRM')
+        res_file_path = os.path.join(self.tables_out_path, 'owners.csv')
+        self._get_simple_ds(res_file_path, OWNER_PK, client_service.get_owners, recent)
+
+        logging.info('Extracting Contacts from HubSpot CRM')
+        self.get_contacts(client_service, start_date)
+
+        logging.info('Extracting Deals from HubSpot CRM')
+        self.get_deals(client_service, start_date)
+
+        logging.info('Extracting Pipelines from HubSpot CRM')
+        self.get_pipelines(client_service)
+
+    def _get_simple_ds(self, res_file_path, pkey, ds_getter, *fpars):
+        """
+        Generic class to get simple objects
+        :param res_file_path:
+        :param pkey:
+        :param ds_getter:
+        :return:
+        """
+        for res in ds_getter(*fpars):
+            self.output_file(res, res_file_path, res.columns)
+
+        # store manifest
+        self.configuration.write_table_manifest(file_name=res_file_path, primary_key=pkey, incremental=True)
+
+    # CONTACTS
+    def get_contacts(self, client: HubspotClientService, start_time):
+        res_file_path = os.path.join(self.tables_out_path, 'contacts.csv')
+        for res in client.get_contacts(start_time):
+            self._store_contact_submission_and_list(res)
+            res.drop(['form-submissions', 'list-memberships'], 1)
+            self.output_file(res, res_file_path, res.columns)
+
+        # store manifests
+        self.configuration.write_table_manifest(file_name=res_file_path, primary_key=CONTACT_PK, incremental=True)
+        c_subform_path = os.path.join(self.tables_out_path, 'contacts_form_submissions.csv')
+        c_lists_path = os.path.join(self.tables_out_path, 'contacts_lists.csv')
+        self.configuration.write_table_manifest(file_name=c_subform_path, primary_key=C_SUBMISSION_PK, incremental=True)
+        self.configuration.write_table_manifest(file_name=c_lists_path, primary_key=CONTACT_LIST_PK, incremental=True)
+
+    def _store_contact_submission_and_list(self, contacts):
+
+        c_subform_path = os.path.join(self.tables_out_path, 'contacts_form_submissions.csv')
+        c_lists_path = os.path.join(self.tables_out_path, 'contacts_lists.csv')
+        # Create table with Contact's form submissions and lists and drop column afterwards
+        for index, row in contacts.iterrows():
+
+            if len(row['form-submissions']) > 0:
+                temp_contacts_sub_forms = pd.DataFrame(row['form-submissions'])
+                temp_contacts_sub_forms['CONTACT_ID'] = row['canonical-vid']
+                # save res
+                self.output_file(temp_contacts_sub_forms, c_subform_path, temp_contacts_sub_forms.columns)
+
+            if len(row['list-memberships']) > 0:
+                temp_contacts_lists = pd.DataFrame(row['list-memberships'])
+                temp_contacts_lists['CONTACT_ID'] = row['canonical-vid']
+                # save res
+                self.output_file(temp_contacts_lists, c_lists_path, temp_contacts_lists.columns)
+
+    # DEALS
+    def get_deals(self, client: HubspotClientService, start_time):
+        logging.info('Extracting Companies from HubSpot CRM')
+        res_file_path = os.path.join(self.tables_out_path, 'deals.csv')
+        for res in client.get_deals(start_time):
+            self.output_file(res, res_file_path, res.columns)
+            self._store_deals_stage_hist_and_list(res)
+
+        # store manifests
+        self.configuration.write_table_manifest(file_name=res_file_path, primary_key=DEAL_PK, incremental=True)
+        stage_hist_path = os.path.join(self.tables_out_path, 'deals_stage_history.csv')
+        c_lists_path = os.path.join(self.tables_out_path, 'deals_contacts_list.csv')
+        self.configuration.write_table_manifest(file_name=stage_hist_path, primary_key=DEAL_STAGE_HIST_PK,
+                                                incremental=True)
+        self.configuration.write_table_manifest(file_name=c_lists_path, primary_key=DEAL_C_LIST_PK, incremental=True)
+
+    def _store_deals_stage_hist_and_list(self, deals):
+
+        stage_hist_path = os.path.join(self.tables_out_path, 'deals_stage_history.csv')
+        c_lists_path = os.path.join(self.tables_out_path, 'deals_contacts_list.csv')
+        # Create table with Deals' Stage History & Deals' Contacts List
+
+        for index, row in deals.iterrows():
+
+            if str(row['properties.dealstage.versions']) != 'nan' and len(row['properties.dealstage.versions']) > 0:
+                temp_stage_history = pd.DataFrame(row['properties.dealstage.versions'])
+                temp_stage_history['DEAL_ID'] = row['dealId']
+                self.output_file(temp_stage_history, stage_hist_path, temp_stage_history.columns)
+
+            if len(row['associations.associatedVids']) != '[]':
+                temp_deals_contacts_list = pd.DataFrame(row['associations.associatedVids'],
+                                                        columns=['Contact_ID'])
+                temp_deals_contacts_list['Deal_ID'] = row['dealId']
+                self.output_file(temp_deals_contacts_list, c_lists_path, temp_deals_contacts_list.columns)
+
+    # PIPELINES
+    def get_pipelines(self, client: HubspotClientService):
+        logging.info('Extracting Companies from HubSpot CRM')
+        res_file_path = os.path.join(self.tables_out_path, 'pipelines.csv')
+        for res in client.get_pipelines():
+            self.output_file(res, res_file_path, res.columns)
+            self._store_pipeline_stages(res)
+
+        # store manifests
+        self.configuration.write_table_manifest(file_name=res_file_path, primary_key=PIPELINE_PK, incremental=True)
+
+        stage_hist_path = os.path.join(self.tables_out_path, 'pipeline_stages.csv')
+        self.configuration.write_table_manifest(file_name=stage_hist_path, primary_key=PIPELINE_STAGE_PK,
+                                                incremental=True)
+
+    def _store_pipeline_stages(self, pipelines):
+
+        stage_hist_path = os.path.join(self.tables_out_path, 'pipeline_stages.csv')
+        # Create table with Pipelines' Stages.
+        for index, row in pipelines.iterrows():
+
+            if len(row['stages']) > 0:
+                temp_pipelines_stages = pd.DataFrame(row['stages'])
+                temp_pipelines_stages['PIPELINE_ID'] = row['pipelineId']
+                self.output_file(temp_pipelines_stages, stage_hist_path, temp_pipelines_stages.columns)
+
+    def output_file(self, data_output, file_output, column_headers):
+        """
+        Output the dataframe input to destination file
+        Append to the file if file does not exist
+        * row by row
+        """
+        if not os.path.isfile(file_output):
+            with open(file_output, 'w+', encoding='utf-8') as b:
+                data_output.to_csv(b, index=False, columns=column_headers)
+            b.close()
+        else:
+            with open(file_output, 'a', encoding='utf-8') as b:
+                data_output.to_csv(b, index=False, header=False, columns=column_headers)
+            b.close()
+
+
+"""
+        Main entrypoint
+"""
+if __name__ == "__main__":
+    comp = Component()
+    comp.run()
---
+++ src/hubspot/client_service.py
@@ -0,0 +1,228 @@
+import json
+from _datetime import timedelta
+from collections.abc import Iterable
+from datetime import datetime
+
+import pandas as pd
+from kbc.client_base import HttpClientBase
+from pandas.io.json import json_normalize
+
+CAMPAIGNS = 'email/public/v1/campaigns/'
+
+LISTS = 'contacts/v1/lists'
+
+ENGAGEMENTS_PAGED = 'engagements/v1/engagements/paged'
+ENGAGEMENTS_PAGED_SINCE = 'engagements/v1/engagements/recent/modified'
+
+EMAIL_EVENTS = 'email/public/v1/events'
+
+CAMPAIGNS_BY_ID = 'email/public/v1/campaigns/by-id'
+CAMPAIGNS_BY_ID_RECENT = 'email/public/v1/campaigns'
+
+DEALS_ALL = 'deals/v1/deal/paged'
+DEALS_RECENT = 'deals/v1/deal/recent/modified'
+
+COMPANIES_ALL = 'companies/v2/companies/paged'
+COMPANIES_RECENT = 'companies/v2/companies/recent/modified'
+
+MAX_RETRIES = 10
+BASE_URL = 'https://api.hubapi.com/'
+
+# endpoints
+CONTACTS_ALL = 'contacts/v1/lists/all/contacts/all'
+CONTACTS_RECENT = 'contacts/v1/lists/recently_updated/contacts/recent'
+
+
+class HubspotClientService(HttpClientBase):
+
+    def __init__(self, token):
+        HttpClientBase.__init__(self, base_url=BASE_URL, max_retries=MAX_RETRIES, backoff_factor=0.3,
+                                status_forcelist=(429, 500, 502, 504), default_params={"hapikey": token})
+
+    def _get_paged_result_pages(self, endpoint, parameters, res_obj_name, limit_attr, offset_req_attr, offset_resp_attr,
+                                has_more_attr, offset, limit):
+        final_df = pd.DataFrame()
+
+        has_more = True
+        while has_more:
+            parameters[offset_req_attr] = offset
+            parameters[limit_attr] = limit
+
+            req = self.get_raw(self.base_url + endpoint, params=parameters)
+            resp_text = str.encode(req.text, 'utf-8')
+            req_response = json.loads(resp_text)
+
+            if req_response[has_more_attr]:
+                has_more = True
+            else:
+                has_more = False
+            offset = req_response[offset_resp_attr]
+            yield final_df.append(json_normalize(req_response[res_obj_name]))
+
+    def _get_all_pages_result(self, endpoint, parameters, res_obj_name, limit_attr, offset_attr, has_more_attr, offset,
+                              limit):
+        final_df = pd.DataFrame()
+
+        has_more = True
+        while has_more:
+            parameters[offset_attr] = offset
+            parameters[limit_attr] = limit
+
+            req = self.get_raw(self.base_url + endpoint, params=parameters)
+            resp_text = str.encode(req.text, 'utf-8')
+            req_response = json.loads(resp_text)
+
+            if req_response[has_more_attr]:
+                has_more = True
+            else:
+                has_more = False
+            offset = req_response[offset_attr]
+            return final_df.append(json_normalize(req_response[res_obj_name]))
+
+    def get_contacts(self, start_time=None) -> Iterable:
+        """
+        Get either all available contacts or recent ones specified by start_time.
+
+        API supports more options, possible to extend in the future
+        :param start_time:
+        :return: generator object with all available pages
+        """
+        offset = -1
+        contact_properties = ['hs_facebookid', 'hs_linkedinid', 'ip_city', 'ip_country',
+                              'ip_country_code', 'newsletter_opt_in', 'firstname', 'linkedin_profile',
+                              'lastname', 'email', 'mobilephone', 'phone', 'city',
+                              'country', 'region', 'jobtitle', 'company', 'website', 'numemployees',
+                              'industry', 'associatedcompanyid', 'hs_lead_status', 'lastmodifieddate',
+                              'source', 'hs_email_optout', 'twitterhandle', 'lead_type',
+                              'hubspot_owner_id', 'notes_last_updated', 'hs_analytics_source', 'opt_in',
+                              'createdate', 'hs_twitterid', 'lifecyclestage']
+
+        parameters = {'property': contact_properties, 'formSubmissionMode': 'all', 'showListMemberships': 'true'}
+
+        # hubspot api allows only 30 days back
+        if start_time and (datetime.utcnow() - start_time).days >= 30:
+            start_time = datetime.now() + timedelta(-30)
+
+        if start_time:
+            parameters['propertyMode'] = 'value_and_history'
+            return self._get_paged_result_pages(CONTACTS_RECENT, parameters, 'contacts', 'count', 'timeOffset',
+                                                'time-offset', 'has-more', int(start_time.timestamp() * 1000), 100)
+        else:
+            return self._get_paged_result_pages(CONTACTS_ALL, parameters, 'contacts', 'count', 'vidOffset',
+                                                'vid-offset', 'has-more', offset, 100)
+
+    def get_companies(self, recent=False):
+
+        offset = 0
+        company_properties = ['about_us', 'name', 'phone', 'facebook_company_page',
+                              'city', 'country', 'website', 'numberofemployees',
+                              'industry', 'annualrevenue', 'linkedin_company_page',
+                              'hs_lastmodifieddate', 'hubspot_owner_id', 'notes_last_updated', 'description',
+                              'createdate', 'numberofemployees', 'about_us', 'hs_lead_status', 'founded_year',
+                              'twitterhandle',
+                              'linkedinbio']
+
+        parameters = {'properties': company_properties}
+
+        if recent:
+            return self._get_paged_result_pages(COMPANIES_RECENT, parameters, 'results', 'count', 'offset', 'offset',
+                                                'hasMore',
+                                                offset, 200)
+        else:
+            return self._get_paged_result_pages(COMPANIES_ALL, parameters, 'companies', 'limit', 'offset', 'offset',
+                                                'has-more',
+                                                offset, 250)
+
+    def get_deals(self, start_time=None) -> Iterable:
+        offset = 0
+        deal_properties = ['authority', 'budget', 'campaign_source', 'hs_analytics_source', 'hs_campaign',
+                           'hs_lastmodifieddate', 'need', 'partner_name', 'timeframe', 'dealname', 'amount',
+                           'closedate', 'pipeline',
+                           'createdate', 'engagements_last_meeting_booked', 'dealtype', 'hs_createdate', 'description',
+                           'start_date', 'closed_lost_reason', 'closed_won_reason', 'end_date', 'lead_owner',
+                           'tech_owner',
+                           'service_amount', 'contract_type', 'hubspot_owner_id', 'partner_name', 'notes_last_updated']
+
+        parameters = {'properties': deal_properties, 'propertiesWithHistory': 'dealstage',
+                      'includeAssociations': 'true'}
+        if start_time:
+            parameters['since'] = int(start_time.timestamp() * 1000)
+            return self._get_paged_result_pages(DEALS_RECENT, parameters, 'results', 'count', 'offset', 'offset',
+                                                'hasMore',
+                                                offset, 100)
+        else:
+            return self._get_paged_result_pages(DEALS_ALL, parameters, 'deals', 'limit', 'offset', 'offset', 'hasMore',
+                                                offset, 250)
+
+    def get_campaigns(self, recent=False):
+        final_df = pd.DataFrame()
+        if recent:
+            url = CAMPAIGNS_BY_ID_RECENT
+        else:
+            url = CAMPAIGNS_BY_ID
+
+        for res in self._get_paged_result_pages(url, {}, 'campaigns', 'limit', 'offset', 'offset', 'hasMore', None,
+                                                1000):
+
+            for index, row in res.iterrows():
+                req = self.get_raw(self.base_url + CAMPAIGNS + str(row['id']))
+                req_response = req.json()
+
+                final_df = final_df.append(json_normalize(req_response))
+
+            yield final_df[['counters.open', 'counters.click', 'id', 'name']]
+
+    def get_email_events(self, start_date: datetime) -> Iterable:
+        offset = ''
+
+        parameters = {'eventType': 'OPEN', 'startTimestamp': int(start_date.timestamp() * 1000)}
+        for open_ev in self._get_paged_result_pages(EMAIL_EVENTS, parameters, 'events', 'limit', 'offset', 'offset',
+                                                    'hasMore',
+                                                    offset, 1000):
+            yield open_ev
+
+        parameters = {'eventType': 'CLICK', 'startTimestamp': int(start_date.timestamp() * 1000)}
+        for click_ev in self._get_paged_result_pages(EMAIL_EVENTS, parameters, 'events', 'limit', 'offset', 'offset',
+                                                     'hasMore',
+                                                     offset, 1000):
+            yield click_ev
+
+    def get_activities(self, start_time: datetime) -> Iterable:
+        offset = 0
+
+        if start_time:
+            pages = self._get_paged_result_pages(ENGAGEMENTS_PAGED_SINCE, {"since": int(start_time.timestamp() * 1000)},
+                                                 'results', 'count', 'offset', 'hasMore', offset, 250)
+        else:
+            pages = self._get_paged_result_pages(ENGAGEMENTS_PAGED, {}, 'results', 'limit', 'offset', 'offset',
+                                                 'hasMore',
+                                                 offset, 250)
+        for pg_res in pages:
+            pg_res.drop(['metadata.text', 'metadata.html'], 1)
+            yield pg_res
+
+    def get_lists(self):
+        offset = 0
+
+        return self._get_paged_result_pages(LISTS, {}, 'lists', 'limit', 'offset', 'offset', 'has-more',
+                                            offset, 250)
+
+    def get_pipelines(self, include_inactive=None):
+        final_df = pd.DataFrame()
+
+        req = self.get_raw('https://api.hubapi.com/deals/v1/pipelines', params={'include_inactive': include_inactive})
+        req_response = req.json()
+
+        final_df = final_df.append(json_normalize(req_response))
+
+        return [final_df]
+
+    def get_owners(self, include_inactive=None):
+        final_df = pd.DataFrame()
+
+        req = self.get_raw('https://api.hubapi.com/owners/v2/owners/', params={'include_inactive': include_inactive})
+        req_response = req.json()
+
+        final_df = final_df.append(json_normalize(req_response))
+
+        return [final_df]
