08f7505aa5e2e82af15561e8d832f2ef6c594f38
David Esner <esnerda@gmail.com>
2019-01-13 18:40:59 -0800

    UPDATE pipelines - sync dev portal properties

--- scripts/update_dev_portal_properties.sh
+++ scripts/update_dev_portal_properties.sh
@@ -7,6 +7,7 @@ docker pull quay.io/keboola/developer-portal-cli-v2:latest
 # Update properties in Keboola Developer Portal
 echo "Updating long description"
 value=`cat component_config/component_long_description.md`
+echo "$value"
 docker run --rm \
     -e KBC_DEVELOPERPORTAL_USERNAME \
     -e KBC_DEVELOPERPORTAL_PASSWORD \
@@ -15,24 +16,33 @@ docker run --rm \
 
 echo "Updating config schema"
 value=`cat component_config/configSchema.json`
-docker run --rm \
-    -e KBC_DEVELOPERPORTAL_USERNAME \
-    -e KBC_DEVELOPERPORTAL_PASSWORD \
-    quay.io/keboola/developer-portal-cli-v2:latest \
-    update-app-property ${KBC_DEVELOPERPORTAL_VENDOR} ${KBC_DEVELOPERPORTAL_APP} configurationSchema --value=${value}
+echo "$value"
+if [ -z "$value" ]
+then
+    docker run --rm \
+        -e KBC_DEVELOPERPORTAL_USERNAME \
+        -e KBC_DEVELOPERPORTAL_PASSWORD \
+        quay.io/keboola/developer-portal-cli-v2:latest \
+        update-app-property ${KBC_DEVELOPERPORTAL_VENDOR} ${KBC_DEVELOPERPORTAL_APP} configurationSchema --value=${value}
 
 echo "Updating config description"
 value=`cat component_config/configuration_description.md`
-docker run --rm \
-    -e KBC_DEVELOPERPORTAL_USERNAME \
-    -e KBC_DEVELOPERPORTAL_PASSWORD \
-    quay.io/keboola/developer-portal-cli-v2:latest \
-    update-app-property ${KBC_DEVELOPERPORTAL_VENDOR} ${KBC_DEVELOPERPORTAL_APP} configurationDescription --value=${value}
+echo "$value"
+if [ -z "$value" ]
+then
+    docker run --rm \
+        -e KBC_DEVELOPERPORTAL_USERNAME \
+        -e KBC_DEVELOPERPORTAL_PASSWORD \
+        quay.io/keboola/developer-portal-cli-v2:latest \
+        update-app-property ${KBC_DEVELOPERPORTAL_VENDOR} ${KBC_DEVELOPERPORTAL_APP} configurationDescription --value=${value}
 
 echo "Updating short description"
 value=`cat component_config/component_short_description.md`
-docker run --rm \
-    -e KBC_DEVELOPERPORTAL_USERNAME \
-    -e KBC_DEVELOPERPORTAL_PASSWORD \
-    quay.io/keboola/developer-portal-cli-v2:latest \
-    update-app-property ${KBC_DEVELOPERPORTAL_VENDOR} ${KBC_DEVELOPERPORTAL_APP} shortDescription --value=${value}
+echo "$value"
+if [ -z "$value" ]
+then
+    docker run --rm \
+        -e KBC_DEVELOPERPORTAL_USERNAME \
+        -e KBC_DEVELOPERPORTAL_PASSWORD \
+        quay.io/keboola/developer-portal-cli-v2:latest \
+        update-app-property ${KBC_DEVELOPERPORTAL_VENDOR} ${KBC_DEVELOPERPORTAL_APP} shortDescription --value=${value}
