4b969ec88de74852af2579e354f59fd1222fbfcb
David Esner <esnerda@gmail.com>
2019-01-13 18:27:33 -0800

    UPDATE pipelines - sync dev portal properties

--- update_dev_portal_properties.sh
+++ update_dev_portal_properties.sh
@@ -6,11 +6,13 @@ docker pull quay.io/keboola/developer-portal-cli-v2:latest
 
 # Update properties in Keboola Developer Portal
 echo "Updating long description"
+value=`cat component_config/component_long_description.md`
+echo "$value"
 docker run --rm \
     -e KBC_DEVELOPERPORTAL_USERNAME \
     -e KBC_DEVELOPERPORTAL_PASSWORD \
     quay.io/keboola/developer-portal-cli-v2:latest \
-    update-app-property ${KBC_DEVELOPERPORTAL_VENDOR} ${KBC_DEVELOPERPORTAL_APP} longDescription --value-from-file=component_config/component_long_description.md
+    update-app-property ${KBC_DEVELOPERPORTAL_VENDOR} ${KBC_DEVELOPERPORTAL_APP} longDescription --value=${value}
 
 echo "Updating config schema"
 docker run --rm \
