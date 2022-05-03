255db523ba61bd87e17e2337ea503a5c85660be0
David Esner <david@keboola.com>
2022-05-03 15:06:25 +0200

    HOTFIX v3 pagination

--- src/hubspot_api/client_service.py
+++ src/hubspot_api/client_service.py
@@ -194,7 +194,7 @@ class HubspotClientService(HttpClient):
             self._check_http_result(req, endpoint)
             req_response = self._parse_response_text(req, endpoint, {offset_req_attr: offset, limit_attr: limit})
 
-            if req_response.get(has_more_attr) or not req_response.get(res_obj_name):
+            if req_response.get(has_more_attr) or req_response.get(res_obj_name):
                 has_more = True
                 offset = req_response[offset_resp_attr]
             else:
